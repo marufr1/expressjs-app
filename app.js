54e008bb13fbdc2d457294e0c50db0138822ce93
marufr1 <marufrahman923@gmail.com>
2022-03-22 22:07:47 +0700

    Add sequelize, user control, modify some files

--- app.js
+++ app.js
@@ -2,13 +2,37 @@ const express = require('express')
 const router = require('./src/routes/app.route')
 const app = express()
 const port = 3000
+const { Sequelize } = require('sequelize')
+const config = require('./config/config.json')
+
+module.exports = config
 
 // by Muhammad Ma'ruf Rahman
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use('/', router)
+appInit(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  const db = new Sequelize(
+    config.development.database,
+    config.development.username,
+    config.development.password, {
+      host: config.development.host,
+      port: 5432,
+      dialect: config.development.dialect
+    })
+  db.authenticate()
+    .then(console.log('Success connected to database'))
+    .catch(err => {
+      console.log('Failed connect to database', err)
+      process.exit()
+    })
 })
+
+function appInit(app) {
+  app.use(express.json())
+  app.use(express.urlencoded({
+    extended: false
+  }))
+  app.use('/', router)
+}
