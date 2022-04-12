e677ec8c6a1bd41abffb60e77434f3cb98d7a6b4
marufr1 <marufrahman923@gmail.com>
2022-04-12 19:25:14 +0700

    added unit testing with jest

---
+++ tests/app.production.test.js
@@ -0,0 +1,61 @@
+const {Product, user} = require('../models/index')
+const ProductController = require('../src/controllers/product.controller')
+const UserController = require('../src/controllers/user.controller')
+const httpMocks = require('node-mocks-http')
+
+jest.mock('../models/index')
+
+let req, res, next
+
+beforeEach(() => {
+    req = httpMocks.createRequest()
+    req.headers["auth"]
+    res = httpMocks.createResponse()
+})
+describe('ProductController.postProduct', () => {
+    it('should return 200', async () => {
+        Product.create.mockResolvedValue({
+            name: 'hello'
+        })
+        await ProductController.postProduct(req, res)
+        expect(res.statusCode).toBe(200)
+    })
+    it('should return error', async () => {
+        const errData = {
+            status: "FAILED",
+            message: "Failed for add product to database"
+        }
+
+        const rejected = Promise.reject(errData)
+
+        Product.create.mockResolvedValue(rejected)
+        await ProductController.postProduct(req, res)
+        expect(res.statusCode).toBe(503)
+    })
+})
+
+beforeEach(() => {
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
+})
+describe('UserController.postSignIn', () => {
+    it('should return 200', async () => {
+        user.findOne.mockResolvedValue({
+            email: 'hello@world.net'
+        })
+        UserController.postSignIn(req, res)
+        expect(res.statusCode).toBe(200)
+    })
+    it('should return error', async () => {
+        const errData = {
+            status: "FAILED",
+            message: "Failed for add product to database"
+        }
+
+        const rejected = Promise.reject(errData)
+
+        user.findOne.mockResolvedValue(rejected)
+        await UserController.postSignIn(req, res)
+        expect(res.statusCode).toBe(503)
+    })
+})
