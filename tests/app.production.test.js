804570288de60da0048533aeec0c1f2e5490ef60
marufr1 <marufrahman923@gmail.com>
2022-04-16 12:53:51 +0700

    Modify build.yml & app.production.test.js

--- tests/app.production.test.js
+++ tests/app.production.test.js
@@ -59,3 +59,51 @@ describe('UserController.postSignIn', () => {
         expect(res.statusCode).toBe(503)
     })
 })
+
+beforeEach(() => {
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
+})
+describe('UserController.getUser', () => {
+    it('should return 200', async () => {
+        user.findAll.mockResolvedValue()
+        UserController.getUser(req, res)
+        expect(res.statusCode).toBe(200)
+    })
+    it('should return error', async () => {
+        const errData = {
+            status: "FAILED",
+            message: "Failed for load users table"
+        }
+
+        const rejected = Promise.reject(errData)
+
+        user.findAll.mockResolvedValue(rejected)
+        await UserController.getUser(req, res)
+        expect(res.statusCode).toBe(503)
+    })
+})
+
+beforeEach(() => {
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
+})
+describe('ProductController.getProduct', () => {
+    it('should return 200', async () => {
+        Product.findAll.mockResolvedValue()
+        ProductController.getProduct(req, res)
+        expect(res.statusCode).toBe(200)
+    })
+    it('should return error', async () => {
+        const errData = {
+            status: "FAILED",
+            message: "Failed for load users table"
+        }
+
+        const rejected = Promise.reject(errData)
+
+        Product.findAll.mockResolvedValue(rejected)
+        await ProductController.getProduct(req, res)
+        expect(res.statusCode).toBe(503)
+    })
+})
